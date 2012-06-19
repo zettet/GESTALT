5b7a8eb37a2f7dd553ef16f8159a464b4e27eaeb
Hans Vorsteveld <hans@hans-laptop.(none)>
2012-06-19 00:03:26 -0400

    Remove GESTALT (capitilized) directory

--- GESTALT/cache_map.py
+++
@@ -1,3 +0,0 @@
-#Here is a file for a custom data structure. It could be as complicated 
-# or as simple as we want it to be, but I think we need to segment it 
-# out into its own class so that it's easier to maintain 
--- GESTALT/scanner_cpp.py
+++
@@ -1,3 +0,0 @@
-# for example, he is where, if the scanner decides that it needs to
-# run syntax highlighting on a c++ file, it could pop this guy into
-# it's engine and run stuff on that file tree.
