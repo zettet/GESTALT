3a6641e3ecfb48cd7d8f550d69a80858aa96a7c0
Hans Vorsteveld <hans@hans-laptop.(none)>
2012-06-19 00:02:24 -0400

    Directory needs to be lowercase

---
+++ gestalt/scanner_cpp.py
@@ -0,0 +1,3 @@
+# for example, he is where, if the scanner decides that it needs to
+# run syntax highlighting on a c++ file, it could pop this guy into
+# it's engine and run stuff on that file tree.
